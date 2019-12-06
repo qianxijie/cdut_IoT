f4af0230b01fd1521f3d84698251ef15d64b1a86
ubuntu <farsight@ubuntu.(none)>
2019-12-06 14:31:46 +0800

    ceshi

---
+++ myhead.h
@@ -0,0 +1,19 @@
+/*===============================================================
+*   Copyright (C) 2019 All rights reserved.
+*   
+*   文件名称：myhead.h
+*   创 建 者：liujing
+*   创建日期：2019年12月06日
+*   描    述：
+*
+*   更新日志：
+*
+================================================================*/
+#ifndef _MYHEAD_H
+#define _MYHEAD_H
+#include <sys/socket.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
